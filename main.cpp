5b9922fc5bc0889bb65e85bbed7cd38b544ff454
HaDeutscher <ha@deut.sh>
2020-02-01 00:39:32 +0200

    Compiling now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "cpq.tab.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+extern FILE *yyin;
+
+int main(int argc, const char *argv[])
+{
+    if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            perror("fopen");
+            return -1;
+        }
+    } else {
+        cout << "Usage: tree <in_file>" << endl;
+        return -1;
+    }
+    yyparse();
     return 0;
 }
